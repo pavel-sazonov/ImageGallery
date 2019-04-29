fd5004ca631ea2c6eeec050cfd97569d963d6ce7
pavel-sazonov <wismyl@ya.ru>
2019-04-29 21:04:58 +0300

    Initial Commit

---
+++ ImageGallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageGallery
+//
+//  Created by Pavel Sazonov on 29/04/2019.
+//  Copyright © 2019 Pavel Sazonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
