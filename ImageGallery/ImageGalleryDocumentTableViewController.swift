2f8d6ebab713cab86d5f155114b84eab53f10f69
pavel-sazonov <wismyl@ya.ru>
2019-05-08 14:20:54 +0300

    user can delete gallery

--- ImageGallery/ImageGalleryDocumentTableViewController.swift
+++ ImageGallery/ImageGalleryDocumentTableViewController.swift
@@ -31,6 +31,17 @@ class ImageGalleryDocumentTableViewController: UITableViewController {
 
         return cell
     }
+
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            imageGalleryDocuments.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
     
     // MARK: - Create New Gallery
     
@@ -38,27 +49,6 @@ class ImageGalleryDocumentTableViewController: UITableViewController {
         imageGalleryDocuments += ["Untitled".madeUnique(withRespectTo: imageGalleryDocuments)]
         tableView.reloadData()
     }
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
 
     /*
     // Override to support rearranging the table view.
