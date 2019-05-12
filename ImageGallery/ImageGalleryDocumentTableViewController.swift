45d8145fc31aaa2ea4b300ac0d60f21d89627ffb
pavel-sazonov <wismyl@ya.ru>
2019-05-12 21:54:23 +0300

    add segue from table view to image gallery

--- ImageGallery/ImageGalleryDocumentTableViewController.swift
+++ ImageGallery/ImageGalleryDocumentTableViewController.swift
@@ -89,7 +89,7 @@ class ImageGalleryDocumentTableViewController: UITableViewController {
             tableView.insertRows(at: [insertIndexPath], with: .fade)
             handler(true)
         }
-                
+        
         return UISwipeActionsConfiguration(actions: [restoreAction])
     }
 
@@ -108,15 +108,10 @@ class ImageGalleryDocumentTableViewController: UITableViewController {
     }
     */
 
-    /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
 
 }
 
